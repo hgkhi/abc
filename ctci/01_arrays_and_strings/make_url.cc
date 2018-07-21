8244464ad274096fe58ca520a028a550d5b61596
Khoi HOANG <hgkhi@protonmail.com>
2018-07-21 11:54:21 +0900

    [Chapter 1] Solved make_url

---
+++ ctci/01_arrays_and_strings/make_url.cc
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+/**
+ * Runtime: O(n)
+ * Space: O(1)
+ */
+void make_url(char* data, size_t virtual_size, size_t buffer_size)
+{
+	char* p1 = data + virtual_size - 1;
+	char* p2 = data + buffer_size - 1;
+	*(p2--) = '\0';
+	while (p1 < p2) {
+		if (*p1 == ' ') {
+			*(p2--) = '0';
+			*(p2--) = '2';
+			*(p2--) = '%';
+		} else {
+			*(p2--) = *p1;
+		}
+		--p1;
+	}
+}
