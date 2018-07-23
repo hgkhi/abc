e29ec99a91722122f4e2b593b7c066dd1d489cf8
Khoi HOANG <hgkhi@protonmail.com>
2018-07-23 22:14:05 +0900

    [Template] Added bits/stdc++11.h header

---
+++ templates/bits/stdc++.h
@@ -0,0 +1,18 @@
+/**
+ * Includes necessary headers for practicing solving computing problems.
+ */
+
+#include <cmath>
+#include <cstddef>
+#include <algorithm>
+#include <bitset>
+#include <iostream>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
