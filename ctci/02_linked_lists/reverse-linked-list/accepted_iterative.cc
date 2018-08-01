e6959c8845755f3c9d58eeec65117d18c91825d5
Khoi HOANG <hgkhi@protonmail.com>
2018-08-01 23:59:05 +0900

    [Linked List] Solve Reverse Linked List problem

---
+++ ctci/02_linked_lists/reverse-linked-list/accepted_iterative.cc
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+/**
+ * Runtime  O(n)
+ * Space    O(1)
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if (head == nullptr) {
+            return nullptr;
+        }
+        
+        ListNode* iterator = head;
+        ListNode* temp = nullptr;
+        
+        while (iterator->next != nullptr) {
+            temp = iterator->next;
+            iterator->next = iterator->next->next;
+            temp->next = head;
+            head = temp;
+        }
+        
+        return head;
+    }
+};
